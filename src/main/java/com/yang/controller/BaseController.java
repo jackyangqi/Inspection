7aeafcc808f72e3116fd6e29e155ccbf681f2492
jackyangqi <17756550826@163.com>
2021-09-15 18:09:44 +0800

    1

--- src/main/java/com/yang/controller/BaseController.java
+++ src/main/java/com/yang/controller/BaseController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSONObject;
 import com.yang.user.entity.User;
@@ -23,8 +25,13 @@ public class BaseController {
 		return "index";
 	}
 	@RequestMapping("/admin/index")
-	public String index1() {
-		return "index";
+	public ModelAndView index1(Model model) {		
+		User user  = new User();
+		user.setId(1);
+		List<MenuModel> resultList  = permissionService.selectMenuModel(user);
+		model.addAttribute("menu", resultList);
+		ModelAndView view = new ModelAndView("index");		
+		return view;
 	}
 	@RequestMapping("/admin/home")
 	public String home() {
